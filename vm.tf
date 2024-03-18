9891ebb4e774f630e1f5daaa74758eb0131237c6
Raja Sekhar Y <23y.rajasekhar@gmail.com>
2024-03-18 19:13:08 +0530

    test-1

---
+++ vm.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "default" {
+  provider = google
+  name = "worker-1"
+  machine_type = "e2-micro"
+  network_interface {
+    network = "default"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20220712"
+    }
+  }
+  # Some changes require full VM restarts
+  # consider disabling this flag in production
+  #   depending on your needs
+  allow_stopping_for_update = true
+}
