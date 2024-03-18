f779f7d99bf0f3e0351489aee2dc701376e20011
Raja Sekhar Y <23y.rajasekhar@gmail.com>
2024-03-18 20:19:01 +0530

    adding worker 2 & 3

--- vm.tf
+++ vm.tf
@@ -21,6 +21,43 @@ resource "google_compute_instance" "worker-1" {
   }
 }
 
+# Create a Compute Engine instance
+resource "google_compute_instance" "worker-2" {
+  name         = "worker-2"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.external_ip.address
+    }
+  }
+}
+
+# Create a Compute Engine instance
+resource "google_compute_instance" "worker-3" {
+  name         = "worker-3"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.external_ip.address
+    }
+  }
+}
+
+
 output "public_ip" {
   value = google_compute_address.external_ip.address
 }
